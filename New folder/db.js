5b9f137560ba5658ef86b37db4634799ec5572c8
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-09-19 00:20:13 +0600

    added

---
+++ New folder/db.js
@@ -0,0 +1,19 @@
+const todos = require("./data/todos");
+const videos = require("./data/videos");
+const videos2 = require("./data/videos2");
+const tags = require("./data/tags");
+const transactions = require("./data/transactions");
+const users = require("./data/users");
+const conversations = require("./data/conversations");
+const messages = require("./data/messages");
+
+module.exports = {
+  todos,
+  videos,
+  tags,
+  videos2,
+  transactions,
+  users,
+  conversations,
+  messages,
+};
